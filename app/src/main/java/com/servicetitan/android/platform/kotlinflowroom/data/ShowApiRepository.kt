b374f962dd993dba07cfa6b27d0fcfc92c1c01b7
Hardeep Singh <hsingh@servicetitan.com>
2020-07-16 17:47:21 -0700

    Flow vs Suspend

--- app/src/main/java/com/servicetitan/android/platform/kotlinflowroom/data/ShowApiRepository.kt
+++ app/src/main/java/com/servicetitan/android/platform/kotlinflowroom/data/ShowApiRepository.kt
@@ -2,16 +2,25 @@ package com.servicetitan.android.platform.kotlinflowroom.data
 
 import com.servicetitan.android.platform.kotlinflowroom.data.local.ShowDatabase
 import com.servicetitan.android.platform.kotlinflowroom.data.model.Genre
+import com.servicetitan.android.platform.kotlinflowroom.data.model.Response
 import com.servicetitan.android.platform.kotlinflowroom.data.model.Show
 import com.servicetitan.android.platform.kotlinflowroom.data.remote.ShowApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class ShowApiRepository(private val showApi: ShowApi, private val showDatabase: ShowDatabase) {
 
-    fun popularShow(): Flow<List<Show>> =
+    suspend fun popularShowSuspend(refresh: Boolean = false): Response<Show> = showApi.popularShow()
+
+    suspend fun test(): String {
+        delay(5000)
+        return "yes"
+    }
+
+    fun popularShow(refresh: Boolean = false): Flow<List<Show>> =
         flow {
-            if(showDatabase.showDao().allShows().isEmpty()) {
+            if(showDatabase.showDao().allShows().isEmpty() || refresh) {
                 val shows: List<Show> = showApi.popularShow().results
                 showDatabase.showDao().insert(shows)
             }
--- app/src/main/java/com/servicetitan/android/platform/kotlinflowroom/view/MainActivity.kt
+++ app/src/main/java/com/servicetitan/android/platform/kotlinflowroom/view/MainActivity.kt
@@ -2,14 +2,20 @@ package com.servicetitan.android.platform.kotlinflowroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.servicetitan.android.platform.kotlinflowroom.R
 import com.servicetitan.android.platform.kotlinflowroom.adapter.ShowAdapter
+import com.servicetitan.android.platform.kotlinflowroom.data.model.Response
+import com.servicetitan.android.platform.kotlinflowroom.data.model.Show
 import com.servicetitan.android.platform.kotlinflowroom.provideShowRepository
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -24,14 +30,39 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        lifecycleScope.launch {
+//        Scope1 {
+//            val popularShow: Response<Show> = provideShowRepository().popularShowSuspend()
+//            val string = provideShowRepository().test()
+//            Toast.makeText(this@MainActivity, string, Toast.LENGTH_LONG).show()
+//            showAdapter.updateData(popularShow.results)
+//        }
+
+        Scope2 {
             provideShowRepository().popularShow()
-                    .combine(provideShowRepository().genre()) { shows, genre ->
-                        shows.forEach { it.updateGenre(genre) }.let { shows }
-                    }.collect {
-                        showAdapter.updateData(it)
-                    }
+                .combine(provideShowRepository().genre()) { shows, genre ->
+                    shows.forEach { it.updateGenre(genre) }.let { shows }
+                }
+                .progress()
+                .collect { showAdapter.updateData(it) }
         }
+    }
+
+    private fun LifecycleOwner.Scope1(block: suspend CoroutineScope.() -> Unit) =
+        lifecycleScope.launch(block = {
+            showProgress(true)
+            block.invoke(this)
+            showProgress(false)
+        })
+
+    private fun LifecycleOwner.Scope2(block: suspend CoroutineScope.() -> Unit) =
+        lifecycleScope.launch(block = block)
+
+    private fun <T> Flow<T>.progress() =
+        onStart { showProgress(true) }.onCompletion { showProgress(false) }
+
+    private fun showProgress(inProgress: Boolean) {
+        progressBar.visibility = if (inProgress) View.VISIBLE else View.GONE
+        recyclerView.visibility = if (inProgress) View.GONE else View.VISIBLE
     }
 
     private fun setupRecyclerView() {
